a707eaa145b79df9e6aaaa70dc75acb09b3dbb00
sbx_user1059 <sbx_user1059@ip-10-128-166-47.eu-west-2.compute.internal>
2019-04-04 15:56:52 +0000

    Thu Apr 04 15:56:52 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,11 +2,13 @@
 class FizzBuzz
 
   def fizz_buzz(number)
+    p number.to_s.split(//).include?("3")
     return 'fizz buzz' if number % 15 == 0
     return 'fizz' if number % 3 == 0
-    return 'fizz' if number.to_s.split(//).include?(3)
+
     return 'buzz' if number % 5 == 0
     number
   end
 
 end
+
