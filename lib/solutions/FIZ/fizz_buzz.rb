a0dee315465d62abe49e2a5a8f5ea4c1ed5524a1
sbx_user1059 <sbx_user1059@ip-10-128-166-47.eu-west-2.compute.internal>
2019-04-04 15:47:50 +0000

    Thu Apr 04 15:47:50 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,9 +2,12 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    return 'fizz' if number % 3 == 0 
+    return 'fizz buzz' if number % 15 == 0
+    return 'fizz' if number % 3 == 0
+    return 'buzz' if number % 5 == 0
     number
   end
 
 end
+
 
--- spec/FIZ/fizz_buzz_spec.rb
+++ spec/FIZ/fizz_buzz_spec.rb
@@ -9,11 +9,24 @@ describe FizzBuzz do
   describe "#fizz_buzz" do
     it 'returns the number when it is not divisible by 3 or 5' do
       expect(subject.fizz_buzz(1)).to eq 1
+      expect(subject.fizz_buzz(7)).to eq 7
     end
 
     it 'returns fizz when the number is divisible by 3' do
       expect(subject.fizz_buzz(3)).to eq 'fizz'
+      expect(subject.fizz_buzz(12)).to eq 'fizz'
+    end
+
+    it 'returns buzz when the number is divisible by 5' do
+      expect(subject.fizz_buzz(5)).to eq 'buzz'
+      expect(subject.fizz_buzz(20)).to eq 'buzz'
+    end
+
+    it 'returns fizz buzz when the number is divisible by 3 and 5' do
+      expect(subject.fizz_buzz(15)).to eq 'fizz buzz'
+      expect(subject.fizz_buzz(60)).to eq 'fizz buzz'
     end
   end
 end
+
 
