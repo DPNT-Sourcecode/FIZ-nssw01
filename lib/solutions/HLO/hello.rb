c3fbbb32ad036bc21528b16aba4385cddee13672
sbx_user1059 <sbx_user1059@ip-10-128-166-47.eu-west-2.compute.internal>
2019-04-04 15:38:19 +0000

    Thu Apr 04 15:38:19 UTC 2019

--- lib/solutions/HLO/hello.rb
+++ lib/solutions/HLO/hello.rb
@@ -2,9 +2,7 @@
 class Hello
 
   def hello(friend_name)
-    "Hello, World!"
+    "Hello, #{friend_name}!"
   end
 
 end
-
-
--- spec/HLO/hello_spec.rb
+++ spec/HLO/hello_spec.rb
@@ -9,9 +9,11 @@ require_solution 'HLO'
 describe Hello do
   describe '#hello' do
     it 'Should say hello to the world' do
-      expect(subject.hello("Alice")).to eq "Hello, World!"
+      expect(subject.hello("World")).to eq "Hello, World!"
+    end
+
+    it 'Should say hello to the person being greeted' do
+      expect(subject.hello("Alice")).to eq "Hello, Alice!"
     end
   end
 end
-
-
